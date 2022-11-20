f3541253b834ddec3191c1df0377d6ef2742f9cb
FGeaorg <g29f05@gmail.com>
2022-11-20 15:20:02 +0800

    R4

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const helmet = require('helmet');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const bodyParser = require('body-parser');
@@ -20,8 +19,6 @@ app.use(cors);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(helmet());
 
 app.use(requestLogger);
 
